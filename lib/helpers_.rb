ec7e2ba91195b496fd25d2892f3bf66c29f70690
Marin Minnerly <dtreeLive@hotmail.com>
2013-07-05 00:11:18 -0400

    Make proper use of Nanoc Helpers
    Moved format_time method to TimeManipulation module in lib/helpers/
    Added ordinalize method to TimeManipulation
    Helpers.rb includes TimeManipulation and has been renamed with
    an underscore so it loads first.

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -3,21 +3,7 @@ include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Tagging
 
-# ===
-# Formats the standard "Time.now" output to make it nicer to read.
-#
-# Takes two parameters, `format` and `time`, time is what is output
-# by "Time.now" and format is how it should appear.
-#
-# Example use from the Article layout:
-# format_time("%B %-d, %Y", post[:created_at])
-# > July 3, 2013
-#
-# Reference: http://www.ruby-doc.org/core-2.0/Time.html#strftime-method
-# ===
-def format_time(format, time)
-	Time.parse(time.to_s).strftime(format) if !time.nil?
-end
+include TimeManipulation
 
 # http://clarkdave.net/2012/02/building-a-static-blog-with-nanoc/
 def get_post_start(post)
