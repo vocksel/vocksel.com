c42e04eab4a0e86f357e59003dd2db56b105fba3
Marin Minnerly <dtreeLive@hotmail.com>
2013-09-14 16:00:05 -0400

    Edit config.rb

--- config.rb
+++ config.rb
@@ -32,19 +32,6 @@
 ## Helpers
 ##
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 activate :directory_indexes
 
 set :css_dir,    'assets/css'
@@ -53,18 +40,6 @@ set :js_dir,     'assets/js'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
 end
