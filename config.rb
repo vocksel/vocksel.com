da44558e5db5453396c82ecd331a47d104a39fdb
Marin Minnerly <dtreeLive@hotmail.com>
2013-09-16 14:08:56 -0400

    Edit page options in config.rb
    Add the Homepage layout and remove the layout frontmatter from index.

--- config.rb
+++ config.rb
@@ -7,26 +7,18 @@
 #   config.output_style = :compact
 # end
 
+
 ##
 ## Page options, layouts, aliases and proxies
 ##
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# Apply alternate layouts
+page "/index.html", :layout => :homepage
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+with_layout :blog do
+	page "/blog/*"
+end
+
 
 ##
 ## Helpers
