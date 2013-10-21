e5b5a760edb43f18b9e09c1ae70b58697225c704
David Minnerly <dtreeLive@hotmail.com>
2013-10-20 22:04:37 -0400

    Update example use, remove unnessecary If (TimeManipulation.rb)

--- helpers/TimeManipulation.rb
+++ helpers/TimeManipulation.rb
@@ -6,14 +6,19 @@ module TimeManipulation
   # Takes two parameters, `format` and `time`, time is what is output
   # by "Time.now" and format is how it should appear.
   #
-  # Example use from the Article layout:
-  # format_time("%B %-d, %Y", post[:created_at])
-  # > July 3, 2013
+  # Example use with page frontmatter:
+  #
+  #   ---
+  #   date: 2013-10-20 21:58:20 -0400
+  #   ---
+  #
+  #   format_time("%B %-d, %Y", current_page.date)
+  #   > October 20, 2013
   #
   # Reference: http://www.ruby-doc.org/core-2.0/Time.html#strftime-method
   # ===
   def format_time(format, time)
-    Time.parse(time.to_s).strftime(format) if !time.nil?
+    Time.parse(time.to_s).strftime(format)
   end
 
 end
