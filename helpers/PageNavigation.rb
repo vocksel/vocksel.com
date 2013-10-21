640449a9692df684ea78077c947d15a72402cef0
Marin Minnerly <dtreeLive@hotmail.com>
2013-10-21 12:19:45 -0400

    Remove 'if paginate && num_pages > 1' (PageNavigation.rb)
    The if statement is instead used in the markup so that there aren't
    unused classes in the compiled code if it's false.

--- helpers/PageNavigation.rb
+++ helpers/PageNavigation.rb
@@ -6,32 +6,29 @@ module PageNavigation
   #
   # Example use:
   #
-	# 	.blog__nav--articles
-	# 		%p.blog__prev_page
-	# 			= prev_page()
+  # 	- if paginate && num_pages > 1
+	# 		.blog__nav--articles
+	# 			%p.blog__prev_page
+	# 				= prev_page()
 	#
-	# 		%p.blog__next_page
-	# 			= next_page()
+	# 			%p.blog__next_page
+	# 				= next_page()
 	#
   # ===
 
 	def next_page()
-		if paginate && num_pages > 1
-			if next_page
-				link_to "Next page", next_page
-			else
-				puts "Next page"
-			end
+		if next_page
+			link_to "Next page", next_page
+		else
+			puts "Next page"
 		end
 	end
 
 	def prev_page()
-		if paginate && num_pages > 1
-			if prev_page
-				link_to "Previous page", prev_page
-			else
-				puts "Previous page"
-			end
+		if prev_page
+			link_to "Previous page", prev_page
+		else
+			puts "Previous page"
 		end
 	end
 
