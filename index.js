49436d6d3b40356c716c20d5e33bf156b66d5332
David Minnerly <dtreeLive@hotmail.com>
2014-10-29 12:31:23 -0400

    Initialize Metalsmith

---
+++ index.js
@@ -0,0 +1,19 @@
+var Metalsmith = require('metalsmith'),
+    markdown   = require('metalsmith-markdown'),
+    permalinks = require('metalsmith-permalinks'),
+    templates  = require('metalsmith-templates'),
+    sass       = require('metalsmith-sass');
+
+Metalsmith(__dirname)
+  .use(sass())
+  .use(markdown({
+    gfm: true,
+    tables: true
+  }))
+  .use(templates('handlebars'))
+  .use(permalinks({
+    relative: false
+  }))
+  .build(function(err) {
+    if (err) throw err;
+  });
---
+++ src/index.md
@@ -0,0 +1,7 @@
+---
+title: Helllo, World
+template: index.hbt
+permalink: false
+---
+
+Cool stuff coming soon! In the mean time, [check out my Blog](http://blog.davidminnerly.com)
