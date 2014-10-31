f501854b485cb14270f89a6f3b03a9a926b6302f
Marin Minnerly <dtreeLive@hotmail.com>
2014-10-30 20:17:54 -0400

    Add command line argument to serve the build

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var Metalsmith = require('metalsmith'),
     markdown   = require('metalsmith-markdown'),
     permalinks = require('metalsmith-permalinks'),
     templates  = require('metalsmith-templates'),
-    sass       = require('metalsmith-sass');
+    sass       = require('metalsmith-sass'),
+    serve      = require('metalsmith-serve'),
+    watch      = require('metalsmith-watch');
+var argv       = require('yargs').argv;
 
-Metalsmith(__dirname)
+var Smithy = Metalsmith(__dirname);
+
+Smithy
   .use(sass())
   .use(markdown({
     gfm: true,
@@ -13,7 +18,15 @@ Metalsmith(__dirname)
   .use(templates('jade'))
   .use(permalinks({
     relative: false
-  }))
+  }));
+
+if (argv.serve) {
+  Smithy
+    .use(serve())
+    .use(watch());
+}
+
+Smithy
   .build(function(err) {
     if (err) throw err;
   });
