d71418d8b4bb310b9e24952e4cf25df15e5a65f5
Marin Minnerly <dtreeLive@hotmail.com>
2014-11-12 15:52:40 -0500

    Incorporate Metalsmith into the Gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp        = require('gulp');
 var imagemin    = require('gulp-imagemin');
 var sass        = require('gulp-sass');
+var frontMatter = require('gulp-front-matter');
+
+var gulpsmith   = require('gulpsmith');
+var markdown    = require('metalsmith-markdown');
+var templates   = require('metalsmith-templates');
+var permalinks  = require('metalsmith-permalinks');
+
 var connect     = require('connect');
 var del         = require('del');
 var path        = require('path');
 var serveStatic = require('serve-static');
 var runSequence = require('run-sequence');
+var jade        = require('jade');
+var assign      = require('lodash.assign');
 
 var join = path.join;
 
@@ -52,6 +61,31 @@ gulp.task('images', function() {
     .pipe(gulp.dest(paths.dest));
 });
 
+/**
+ * I intend to only use Metalsmith temporarily. It was a great stand-alone
+ * solution, but now that I have the power of Gulp, I'd like to use it solely
+ * for my build system.
+ */
+gulp.task('metalsmith', function() {
+  return gulp.src(join(paths.src, '**/*.md'))
+    .pipe(frontMatter()).on('data', function(file) {
+      assign(file, file.frontMatter);
+      delete file.frontMatter;
+    })
+    .pipe(gulpsmith()
+      .use(markdown({
+        gfm: true,
+        tables: true
+      }))
+      .use(templates({
+        engine: 'jade',
+        directory: 'src/templates'
+      }))
+      .use(permalinks({
+        relative: false
+      })))
+    .pipe(gulp.dest(paths.dest));
+});
 
 // Development
 // =============================================================================
@@ -68,6 +102,8 @@ gulp.task('watch', function() {
   gulp.watch(join(paths.css, '**'), ['styles']);
 
   gulp.watch(join(paths.img, '**'), ['images']);
+
+  gulp.watch(join(paths.src, '**/*.{md,jade}'), ['metalsmith'])
 });
 
 
@@ -76,7 +112,7 @@ gulp.task('watch', function() {
 
 gulp.task('build', function() {
   // Always clean the directory before compiling
-  runSequence('clean', ['styles', 'images']);
+  runSequence('clean', ['styles', 'images', 'metalsmith']);
 });
 
 gulp.task('serve', [
