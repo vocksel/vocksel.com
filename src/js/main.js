2fa37b2aab1925ed4745ec20411c5e5800cf771c
David Minnerly <dtreeLive@hotmail.com>
2014-11-26 13:26:52 -0500

    Disable Skrollr on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 (function() {
   var s = skrollr.init();
+
+  // Trying to get Skrollr to work properly on mobile devices is taking up too
+  // much time. Parallax on mobile is on hold until after v5 is released.
+  if (s.isMobile()) {
+    s.destroy();
+  }
 })();
