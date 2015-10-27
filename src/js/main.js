76d08cd4f53995f83e8c7a916ce8cd02a4c19e2f
David Minnerly <dtreeLive@hotmail.com>
2015-10-27 10:47:51 -0400

    Make images go from gray to color on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
-(function() {
-  var s = skrollr.init();
+/**
+ * Turns images from grayscale to color as you scroll over them.
+ *
+ * You need to apply the .js-color-scroll and .grayscale classes to the image
+ * that you want to use. The first class is the hook we use, and the second is
+ * to ensure images are grayscale by default.
+ *
+ * It can be setup so that .grayscale is applied when the script runs, but
+ * then the images are briefly in color as the page loads.
+ *
+ * This method requires adding two classes in the markup, but the images being
+ * grayscale immediately is very desirable.
+ *
+ * Example:
 
-  // Trying to get Skrollr to work properly on mobile devices is taking up too
-  // much time. Parallax on mobile is on hold until after v5 is released.
-  if (s.isMobile()) {
-    s.destroy();
+    <img class="js-color-scroll  grayscale" src="/img/sample.pmg">
+
+ */
+function colorScroll() {
+  var viewportCenter = "50%";
+  var $photos = $(".js-color-scroll");
+
+  function toggleColor(element) {
+    $(element).toggleClass("grayscale colorize")
   }
-})();
+
+  function waypointHandler(direction) {
+    toggleColor(this.element)
+  }
+
+  $photos.waypoint(waypointHandler, { offset: viewportCenter });
+}
+
+colorScroll()
