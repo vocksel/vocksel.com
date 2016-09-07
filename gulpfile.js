6a348691b5e2dd1c29297db2bfd5ca36f9ee3d27
Marin Minnerly <dtreeLive@hotmail.com>
2016-09-07 10:49:18 -0400

    Use plain functions for non-series/parallel tasks

--- gulpfile.js
+++ gulpfile.js
@@ -31,74 +31,74 @@ var paths = {
 // Compiling
 // =============================================================================
 
-gulp.task(function clean(done) {
+function clean(done) {
   del(paths.dest, done);
-});
+}
 
-gulp.task('styles', function() {
+function styles() {
   return gulp.src(path.join(paths.css, 'main.scss'), { base: paths.src })
     .pipe(plumber())
     .pipe(sass({
       outputStyle: 'compressed'
     }))
     .pipe(gulp.dest(paths.dest));
-});
+}
 
-gulp.task('images', function() {
+function images() {
   return gulp.src(path.join(paths.img, '**'), { base: paths.src })
     .pipe(gulp.dest(paths.dest));
-});
+}
 
-gulp.task('scripts', function() {
+function scripts() {
   return gulp.src(path.join(paths.js, '**'), { base: paths.src })
     .pipe(plumber())
     .pipe(gulp.dest(paths.dest));
-});
+}
 
 // Moving files that aren't processed by the above tasks.
-gulp.task('move', function() {
+function move() {
   return gulp.src(paths.static)
     .pipe(plumber())
     .pipe(gulp.dest(paths.dest));
-});
+}
 
 
 // Development
 // =============================================================================
 
-gulp.task('server', function() {
+function server() {
   connect.server({
     root: paths.dest
   });
-});
+}
 
-gulp.task('watch', function() {
+function watch(done) {
   // All watched tasks should include `.pipe(plumber())` at the beginning of the
   // stream. This prevents you from having to rerun tasks if an error occurs.
   //
   // Very helpful when working with Sass. If you mess up a variable name or
   // @import path, everything is still running fine.
 
-  gulp.watch(paths.static, gulp.parallel('move'));
+  gulp.watch(paths.static, move);
 
   // Gets all Sass files, even the ones in bower_components.
-  gulp.watch(path.join(paths.src, '**/*.scss'), gulp.parallel('styles'));
+  gulp.watch(path.join(paths.src, '**/*.scss'), styles);
 
-  gulp.watch(path.join(paths.img, '**'), gulp.parallel('images'));
+  gulp.watch(path.join(paths.img, '**'), images);
 
-  gulp.watch(path.join(paths.js, '**'), gulp.parallel('scripts'));
+  gulp.watch(path.join(paths.js, '**'), scripts);
 
   done()
-});
+}
 
 
 // Default
 // =============================================================================
 
-gulp.task('compile', gulp.parallel('move', 'styles', 'images', 'scripts'))
+gulp.task('compile', gulp.parallel(move, styles, images, scripts))
 
-gulp.task('build', gulp.series('clean', 'compile'))
+gulp.task('build', gulp.series(clean, 'compile'))
 
-gulp.task('serve', gulp.series('build', 'server', 'watch'));
+gulp.task('serve', gulp.series('build', server, watch))
 
 gulp.task('default', gulp.parallel('serve'))
