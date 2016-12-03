0b18ce2f031c9e7c8e689b50a764c5aa70b659e0
David Minnerly <dtreeLive@hotmail.com>
2016-12-02 20:56:21 -0500

    Set the site repo in the locals

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -30,6 +30,7 @@ const age = year - 1996
 // (e.g. 'self.title')
 const locals = {
   title: 'David Minnerly',
+  repo: 'https://github.com/vocksel/my-website',
   copyright: '&copy; ' + year + ' David Minnerly',
   age: age
 }
