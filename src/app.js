d4ee042cd72a6c7021a41943b40e7942ce0825ea
David Minnerly <dtreeLive@hotmail.com>
2016-12-23 14:13:53 -0500

    Generate the index file with HtmlWebpackPlugin
    Prior to this we were creating an index.html file inside of our destination directory. This was done purely for testing, and now we have a more permanent solution.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,4 @@
 import { setupPhotoWaypoints } from './js/photo-coloring';
 import './css/main.scss'
-import template from './views/index.pug';
-import locals from '../locals.babel.js';
 
-function propagateDocument() {
-  // We're wrapping the locals in a `self` key because pug-loader doesn't seem
-  // to be respecting the query.
-  //
-  // Setting `self: true` for the query should wrap the locals in a `self` key
-  // for us, but it doesn't seem to be working.
-  //
-  // We'll only have Pug in the project for a little while longer, so this is
-  // just a workaround until its replaced.
-  const html = template({ self: locals })
-  document.write(html)
-}
-
-propagateDocument()
 setupPhotoWaypoints()
