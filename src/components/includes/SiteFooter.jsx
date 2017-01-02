8ae76006685b7dac766a6b4dfc0b95294a2aaab5
David Minnerly <dtreeLive@hotmail.com>
2017-01-02 13:03:54 -0500

    Change "Copyright" to the copyright symbol
    This was an accidental change. The copyright symbol is what we were using before.

--- src/components/includes/SiteFooter.jsx
+++ src/components/includes/SiteFooter.jsx
@@ -6,7 +6,7 @@ function SiteFooter(props) {
   return (
     <footer id="bottom" className="page-micro fine-print">
       <ul className="list-inline--delimited">
-        <li>Copyright {currentYear} David Minnerly</li>
+        <li>&copy; {currentYear} David Minnerly</li>
         <li><a href="https://github.com/vocksel/my-website">Website Source</a></li>
         <li>Background from <a href="http://ommwriter.com">OmmWriter</a></li>
       </ul>
