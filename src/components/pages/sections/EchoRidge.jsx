a476fcaa6b3ee9f54d8d9f0e5ae767eac02fdb73
David Minnerly <dtreeLive@hotmail.com>
2017-01-03 10:02:38 -0500

    Make photo components more reusable
    Including <PhotoImage> as a child manually instead of having it hard-coded means we can swap it out for another component later if we want.

--- src/components/pages/sections/EchoRidge.jsx
+++ src/components/pages/sections/EchoRidge.jsx
@@ -4,6 +4,7 @@ import Content from 'components/content/Content.jsx';
 import ContentTitle from 'components/content/ContentTitle.jsx';
 import PhotoWrapper from 'components/photo/PhotoWrapper.jsx';
 import PhotoLink from 'components/photo/PhotoLink.jsx';
+import PhotoImage from 'components/photo/PhotoImage.jsx';
 import PhotoCaption from 'components/photo/PhotoCaption.jsx';
 
 const url = 'http://www.roblox.com/games/13525723/view?rbxp=1343930';
@@ -16,7 +17,10 @@ function EchoRidge(props) {
       <p><a href={url}>Echo Ridge</a> is a 3D recreation of the hub level in Mega Man Star Force 3. I built it over the span of 3 months, and it has quickly become my favorite ROBLOX project to date.</p>
 
       <PhotoWrapper>
-        <PhotoLink image={require('img/projects/echo-ridge/overview.png')} href={url} />
+        <PhotoLink href={url}>
+          <PhotoImage image={require('img/projects/echo-ridge/overview.png')} />
+        </PhotoLink>
+
         <PhotoCaption>A full view of Echo Ridge, featuring the Sky Wave.</PhotoCaption>
       </PhotoWrapper>
 
@@ -25,7 +29,10 @@ function EchoRidge(props) {
       <p>Thanks to ROBLOX's modeling system I was able to achieve a much higher level of detail. This also opened up the possibility to remove all of the seam lines, giving the map a much cleaner appearance.</p>
 
       <PhotoWrapper>
-        <PhotoLink image={require('img/projects/echo-ridge/comparison.png')} href={url} />
+        <PhotoLink href={url}>
+          <PhotoImage image={require('img/projects/echo-ridge/comparison.png')} />
+        </PhotoLink>
+
         <PhotoCaption>Geo's house, before and after.</PhotoCaption>
       </PhotoWrapper>
     </Content>
