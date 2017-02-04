d095ecdf608355aaa7f6d07d512592d2b5c0950a
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-03 20:13:28 -0500

    Modularize EchoRidge and Fireflies components
    Instead of spliting their images, Photo components and page JSX to separate locations, they're now grouped together.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import About from './sections/About.jsx';
-import EchoRidge from './sections/EchoRidge.jsx';
 import Experience from './sections/Experience.jsx';
-import Fireflies from './sections/Fireflies.jsx';
+import EchoRidge from 'components/portfolio/echo-ridge/Page.jsx';
+import Fireflies from 'components/portfolio/fireflies/Page.jsx';
 
 function Home() {
   return (
--- src/components/portfolio/echo-ridge/Page.jsx
+++ src/components/portfolio/echo-ridge/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Content from 'components/content/Content.jsx';
 import ContentTitle from 'components/content/ContentTitle.jsx';
-import { OverviewPhoto, ComparisonPhoto } from 'components/photo/EchoRidgePhotos.jsx';
+import { Photo, PhotoCaption } from 'components/photo/PhotoComponents.jsx';
 
 const url = 'http://www.roblox.com/games/13525723/view?rbxp=1343930';
 
@@ -13,13 +13,17 @@ function EchoRidge(props) {
 
       <p><a href={url}>Echo Ridge</a> is a 3D recreation of the hub level in Mega Man Star Force 3. I built it over the span of 3 months, and it has quickly become my favorite ROBLOX project to date.</p>
 
-      <OverviewPhoto href={url} />
+      <Photo href={url} image={require('./images/overview.png')}>
+        <PhotoCaption>A full view of Echo Ridge, featuring the Sky Wave.</PhotoCaption>
+      </Photo>
 
       <p>I made the first version of Echo Ridge when I was 14, back in October of 2010. Almost 4 and a half years later, I took another look at it and felt it didn't do the original game justice.</p>
 
       <p>Thanks to ROBLOX's modeling system I was able to achieve a much higher level of detail. This also opened up the possibility to remove all of the seam lines, giving the map a much cleaner appearance.</p>
 
-      <ComparisonPhoto href={url} />
+      <Photo href={url} image={require('./images/comparison.png')}>
+        <PhotoCaption>Geo's house, before and after.</PhotoCaption>
+      </Photo>
     </Content>
   )
 }
