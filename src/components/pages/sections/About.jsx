b9ac624962b293d9a8d23fa71ba45a7515024f6f
David Minnerly <dtreeLive@hotmail.com>
2017-02-09 09:23:05 -0500

    ROBLOX to Roblox
    Roblox's name went through a stylistic change recently. This updates our content to reflect it.

--- src/components/pages/sections/About.jsx
+++ src/components/pages/sections/About.jsx
@@ -29,7 +29,7 @@ function About(props) {
 
       <WalkwayPhoto />
 
-      <p>I got my first taste of programming when I was 13. I started out by learning Lua on the online building game <a href={"http://roblox.com"}>ROBLOX</a>. Making my little blocky creations come to life was amazing to me, and it became the foundation for something more.</p>
+      <p>I got my first taste of programming when I was 13. I started out by learning Lua on the online building game <a href={"http://roblox.com"}>Roblox</a>. Making my little blocky creations come to life was amazing to me, and it became the foundation for something more.</p>
 
       <p>Since then I've branched out to learning about web design, picking up on HTML, CSS and JavaScript along the way. I've experimented with creating applications on <a href={"https://angularjs.org"}>AngularJS</a> and <a href={"https://djangoproject.org"}>Django</a>, and both of them were great fun to work with.</p>
     </Content>
