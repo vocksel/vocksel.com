600c7c949973f6eb326323e89f01fe25c816172e
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-09 11:03:34 -0500

    Use a single wrapper to contain everything
    Instead of sectioning off our content, it all exists under a single wrapper now.
    
    Idealy with a single-page website, you'd want IDs on your elements so the user can bookmark sections of the page and navigate using ID-based links.
    
    We're going to be splitting content across multiple pages, and the portfolio is going to be setup in such a way where this kind of bookmarking won't be needed.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 
-import Content from './content/Content.jsx';
 import { Photo, PhotoCaption } from './photo/photo.jsx';
 
 const age = moment().diff('1996-07-15', 'years');
@@ -22,7 +21,7 @@ function WalkwayPhoto(props) {
 
 function About(props) {
   return (
-    <Content id={"about"}>
+    <div>
       <p>I'm a {age}-year-old programming enthusiast living in the state of New York. I aim to make a career out of server administration and network security, and in my spare time I create websites and computer programs.</p>
 
       <p>I frequently work on open-source projects. All of which can be found on my <a href={"https://github.com/voxeldavid/"}>GitHub</a>.</p>
@@ -32,7 +31,7 @@ function About(props) {
       <p>I got my first taste of programming when I was 13. I started out by learning Lua on the online building game <a href={"http://roblox.com"}>Roblox</a>. Making my little blocky creations come to life was amazing to me, and it became the foundation for something more.</p>
 
       <p>Since then I've branched out to learning about web design, picking up on HTML, CSS and JavaScript along the way. I've experimented with creating applications on <a href={"https://angularjs.org"}>AngularJS</a> and <a href={"https://djangoproject.org"}>Django</a>, and both of them were great fun to work with.</p>
-    </Content>
+    </div>
   )
 }
 
