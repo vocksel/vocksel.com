11c8ba3c80c509cfa364aec38b766af21638a6ea
David Minnerly <dtreeLive@hotmail.com>
2017-02-09 11:08:56 -0500

    Remove .page-micro in favor of just applying classes

--- src/components/SiteFooter.jsx
+++ src/components/SiteFooter.jsx
@@ -4,7 +4,7 @@ const currentYear = new Date().getFullYear()
 
 function SiteFooter(props) {
   return (
-    <footer id="bottom" className="page-micro fine-print">
+    <footer id="bottom" className="centered-text fine-print">
       <ul className="list-inline--delimited">
         <li>&copy; {currentYear} David Minnerly</li>
         <li><a href="https://github.com/vocksel/my-website">Website Source</a></li>
