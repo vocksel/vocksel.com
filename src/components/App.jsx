539bae9e6c983bc943fd3bc6c428bc4cd29dca60
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-09 09:26:22 -0500

    Remove Layout component in favor of standard App
    App is the common name used for the component that consolidates everything together for displaying to the DOM.

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Home from './pages/Home.jsx';
+import SiteFooter from './includes/SiteFooter.jsx';
+
+export default function App(props) {
+  return (
+    <div>
+      <Home />
+      <SiteFooter />
+    </div>
+  );
+}
