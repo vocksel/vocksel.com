96ebcb72cec8904648111a3b0f7f717c848adbe3
David Minnerly <dtreeLive@hotmail.com>
2017-02-10 17:51:30 -0500

    Use browserHistory instead of hashHistory
    This comes with the caveat of needing to configure the web server we use to support browserHistory.
    
    webpack-dev-server has the historyApiFallback option which takes care of everything for us, though once deploed to the production server there will be some issues until it's been configured.

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './App';
 import Home from './routes/Home';
 import About from './routes/About';
 
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='/about' component={About} />
