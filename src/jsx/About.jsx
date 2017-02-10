1bd222725252f836bf6bd1ed9cfba2caea357238
David Minnerly <dtreeLive@hotmail.com>
2017-02-10 08:37:27 -0500

    Wrap lines to 80 characters

--- src/jsx/About.jsx
+++ src/jsx/About.jsx
@@ -12,9 +12,8 @@ function WalkwayPhoto(props) {
     <Photo href={url} title={'Visit the Walkway Over the Hudson\'s website.'}
       image={require('img/biking.jpg')}>
 
-      <PhotoCaption>
-        A photo from my trip back home from the <a href={url}>Walkway Over the Hudson</a>.
-      </PhotoCaption>
+      <PhotoCaption>A photo from my trip back home from the
+        <a href={url}>Walkway Over the Hudson</a>.</PhotoCaption>
     </Photo>
   );
 }
@@ -22,15 +21,26 @@ function WalkwayPhoto(props) {
 function About(props) {
   return (
     <div>
-      <p>I'm a {age}-year-old programming enthusiast living in the state of New York. I aim to make a career out of server administration and network security, and in my spare time I create websites and computer programs.</p>
+      <p>I'm a {age}-year-old programming enthusiast living in the state of New
+        York. I aim to make a career out of server administration and network
+        security, and in my spare time I create websites and computer
+        programs.</p>
 
-      <p>I frequently work on open-source projects. All of which can be found on my <a href={"https://github.com/voxeldavid/"}>GitHub</a>.</p>
+      <p>I frequently work on open-source projects. All of which can be found on
+        my <a href={"https://github.com/voxeldavid/"}>GitHub</a>.</p>
 
       <WalkwayPhoto />
 
-      <p>I got my first taste of programming when I was 13. I started out by learning Lua on the online building game <a href={"http://roblox.com"}>Roblox</a>. Making my little blocky creations come to life was amazing to me, and it became the foundation for something more.</p>
+      <p>I got my first taste of programming when I was 13. I started out by
+        learning Lua on the online building game <a href={"http://roblox.com"}>
+        Roblox</a>. Making my little blocky creations come to life was amazing
+        to me, and it became the foundation for something more.</p>
 
-      <p>Since then I've branched out to learning about web design, picking up on HTML, CSS and JavaScript along the way. I've experimented with creating applications on <a href={"https://angularjs.org"}>AngularJS</a> and <a href={"https://djangoproject.org"}>Django</a>, and both of them were great fun to work with.</p>
+      <p>Since then I've branched out to learning about web design, picking up
+        on HTML, CSS and JavaScript along the way. I've experimented with
+        creating applications on <a href={"https://angularjs.org"}>AngularJS</a>
+        and <a href={"https://djangoproject.org"}>Django</a>, and both of them
+        were great fun to work with.</p>
     </div>
   )
 }
--- src/jsx/portfolio/fireflies/Page.jsx
+++ src/jsx/portfolio/fireflies/Page.jsx
@@ -8,22 +8,30 @@ const title = 'Check out Fireflies in person.';
 function Body() {
   return (
     <div>
-      <p><a href={url}>Fireflies</a> is a small application that displays a quote and background image at random every time the page is loaded.</p>
+      <p><a href={url}>Fireflies</a> is a small application that displays a
+        quote and background image at random every time the page is loaded.</p>
 
       <Photo href={url} title={title} image={require('./images/perks.png')}>
         <PhotoCaption>A screenshot of Fireflies in action.</PhotoCaption>
       </Photo>
 
-      <p>It started as an <a href="https://angularjs.org">AngularJS</a> app. It fit my needs very well at the start, but a major drawback to my setup was that new images and quotes were cumbersome to implement.</p>
+      <p>It started as an <a href="https://angularjs.org">AngularJS</a> app. It
+        fit my needs very well at the start, but a major drawback to my setup was
+        that new images and quotes were cumbersome to implement.</p>
 
-      <p>I later fixed this by porting Fireflies to <a href="https://djangoproject.org">Django</a>. This works considerably better, as now when I want to add a new image or quote, I can do it directly from the admin interface.</p>
+      <p>I later fixed this by porting Fireflies to
+        <a href="https://djangoproject.org">Django</a>. This works considerably
+        better, as now when I want to add a new image or quote, I can do it
+        directly from the admin interface.</p>
 
       <Photo href={url} title={title} image={require('./images/stars.png')}>
         <PhotoCaption>Another screenshot of Fireflies, with a different quote
           and background.</PhotoCaption>
       </Photo>
 
-      <p>All of the backgrounds and quotes mean a lot to me, I picked ones that I personally liked, and because of that I hold Fireflies very near to my heart.</p>
+      <p>All of the backgrounds and quotes mean a lot to me, I picked ones that
+        I personally liked, and because of that I hold Fireflies very near to my
+        heart.</p>
     </div>
   )
 }
