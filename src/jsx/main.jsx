4e1d031585536a3474b86b7190910c7f9cdb935d
David Minnerly <dtreeLive@hotmail.com>
2017-02-10 16:35:22 -0500

    Group pages under a 'routes' folder

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './App.jsx';
-import Home from './Home.jsx';
-import About from './About.jsx';
+import Home from './routes/Home.jsx';
+import About from './routes/About.jsx';
 
 ReactDOM.render((
   <Router history={hashHistory}>
