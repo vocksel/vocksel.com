5747542252e5a6c9372bdab62aea664bf564de6e
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-12 12:54:28 -0500

    Fix linked text not having space from surrounding paragraph
    This had to do with the line wrapping. The browser interprets the text as not having spaces between words and links depending on where the <a> tag is wrapped.
    
    For now, built-in line wrapping in your editor will have to do.

--- src/jsx/routes/About.jsx
+++ src/jsx/routes/About.jsx
@@ -6,21 +6,11 @@ const age = moment().diff('1996-07-15', 'years');
 export default function About() {
   return (
     <div>
-      <p>I'm a {age}-year-old programming enthusiast living in the state of New
-        York. I aim to make a career out of server administration and network
-        security, and in my spare time I create websites and computer
-        programs.</p>
+      <p>I'm a {age}-year-old programming enthusiast living in the state of New York. I aim to make a career out of server administration and network security, and in my spare time I create websites and computer programs.</p>
 
-      <p>I got my first taste of programming when I was 13. I started out by
-        learning Lua on the online building game <a href={'http://roblox.com'}>
-        Roblox</a>. Making my little blocky creations come to life was amazing
-        to me, and it became the foundation for something more.</p>
+      <p>I got my first taste of programming when I was 13. I started out by learning Lua on the online building game <a href={'http://roblox.com'}> Roblox</a>. Making my little blocky creations come to life was amazing to me, and it became the foundation for something more.</p>
 
-      <p>Since then I've branched out to learning about web design, picking up
-        on HTML, CSS and JavaScript along the way. I've experimented with
-        creating applications on <a href={'https://angularjs.org'}>AngularJS</a>
-        and <a href={'https://djangoproject.org'}>Django</a>, and both of them
-        were great fun to work with.</p>
+      <p>Since then I've branched out to learning about web design, picking up on HTML, CSS and JavaScript along the way. I've experimented with creating applications on <a href={'https://angularjs.org'}>AngularJS</a> and <a href={'https://djangoproject.org'}>Django</a>, and both of them were great fun to work with.</p>
     </div>
   );
 }
