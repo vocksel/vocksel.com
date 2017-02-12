2ce3c12e09e75e8944f27483eeae76d0045438ad
David Minnerly <dtreeLive@hotmail.com>
2017-02-12 08:30:00 -0500

    Split project descriptions to their own component files

---
+++ src/jsx/routes/portfolio/fireflies/Description.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Description() {
+  return (
+    <div>
+      <p><a href={'http://fireflies.davidminnerly.com'}>Fireflies</a> is a small
+        application that displays a quote and background image at random every
+        time the page is loaded.</p>
+
+      <p>It started as an <a href="https://angularjs.org">AngularJS</a> app. It
+        fit my needs very well at the start, but a major drawback to my setup
+        was that new images and quotes were cumbersome to implement.</p>
+
+      <p>I later fixed this by porting Fireflies to
+        <a href="https://djangoproject.org">Django</a>. This works considerably
+        better, as now when I want to add a new image or quote, I can do it
+        directly from the admin interface.</p>
+
+      <p>All of the backgrounds and quotes mean a lot to me, I picked ones that
+        I personally liked, and because of that I hold Fireflies very near to my
+        heart.</p>
+    </div>
+  )
+}
