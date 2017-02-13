ac72b36ca1a21742391b43b1d127dff71a55578c
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-13 16:29:51 -0500

    Add Mirage Isle to the portfolio

---
+++ src/jsx/routes/portfolio/mirage-isle/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default {
+  name: 'Mirage Isle',
+  slug: 'mirage-isle',
+  released: '2016',
+  tags: [ '3D Modeling', 'Roblox' ],
+  images: [
+    require('./images/overview.png'),
+    require('./images/boat.png'),
+    require('./images/cave.png'),
+    require('./images/cattails.png'),
+  ],
+  description: (
+    <div>
+      <p>Amidst the sea in a location seldom traveled rests Mirage Isle, a small landmass sparsely covered with grass and trees.</p>
+
+      <p>What appears to be a bastion of hope for weary travelers soon reveals itself as not much more than a desert at sea. With no food or drinkable water, sailors have described it as a mirage, hastily taking away what hope it initially gave them.</p>
+
+      <p>A small boat rests atop the water, anchored to the dock. A tent and campfire sit just above. Someone has been here, but given the lack of sustainability, what are the chances they’re still amongst the living?</p>
+
+      <p>I used Mirage Isle as an “in between” project. I would incrementally add on to it when I needed a break from more intensive projects. Modeling the rocks used for the terrain was surprisingly therapeutic. It didn’t require much thought or effort to sculpt a rock that fit in nicely with the existing landscape.</p>
+
+      <p>This was a delightfully relaxing project to work on, with a very appealing end result.</p>
+    </div>
+  )
+};
