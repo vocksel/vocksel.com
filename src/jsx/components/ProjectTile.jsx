2dc3369584fdcf40b2594fe05f0f38d8a8614e90
David Minnerly <dtreeLive@hotmail.com>
2017-02-14 08:55:30 -0500

    Add year and tags below project tiles

--- src/jsx/components/ProjectTile.jsx
+++ src/jsx/components/ProjectTile.jsx
@@ -3,10 +3,16 @@ import { Link } from 'react-router';
 
 export default function ProjectTile(props) {
   return (
-    <Link to={`/projects/${props.slug}`} className="project-tile grid__col grid__col--1-of-2">
-      <h1 className="project-tile__title">{props.name}</h1>
-      <img className="project-tile__image" src={props.images[0]} />
-    </Link>
+    <div className="project-tile grid__col grid__col--1-of-2">
+      <Link to={`/projects/${props.slug}`}>
+        <h1 className="project__title">{props.name}</h1>
+        <img className="project__image" src={props.images[0]} />
+      </Link>
+
+      <div className="project__meta">
+        <p>{props.released} &ndash; {props.tags.join(', ')}</p>
+      </div>
+    </div>
   );
 }
 
