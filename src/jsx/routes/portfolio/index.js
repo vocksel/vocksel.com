a4bb2c47a0f15b8ad1df3efabf2959399da7b8e4
David Minnerly <dtreeLive@hotmail.com>
2017-02-14 18:23:43 -0500

    Add Elixir to the portfolio

--- src/jsx/routes/portfolio/index.js
+++ src/jsx/routes/portfolio/index.js
@@ -2,6 +2,7 @@ export default [
   require('./mirage-isle').default,
   require('./apartment').default,
   require('./echo-ridge').default,
+  require('./elixir').default,
   require('./fireflies').default,
   require('./snowy-mountain').default
 ];
