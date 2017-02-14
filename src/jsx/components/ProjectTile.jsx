e864d6d03516deb4868b4701997d4d7084c9d610
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-13 20:25:01 -0500

    Setup the Toast grid system

--- src/jsx/components/ProjectTile.jsx
+++ src/jsx/components/ProjectTile.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 export default function ProjectTile(props) {
   return (
-    <Link to={`/projects/${props.slug}`} className='project-tile'>
+    <Link to={`/projects/${props.slug}`} className="project-tile grid__col grid__col--1-of-2">
       <h1 className="project-tile__title">{props.name}</h1>
       <img className="project-tile__image" src={props.images[0]} />
     </Link>
