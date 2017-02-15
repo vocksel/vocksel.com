5a11b4f7d4416dc96c561e60123f08ef1698b272
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-14 19:06:23 -0500

    Add Class.lua to the portfolio

---
+++ src/jsx/routes/portfolio/class-lua/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default {
+  name: 'Class.lua',
+  slug: 'class-lua',
+  released: '2016',
+  tags: [ 'Software', 'Lua' ],
+  images: [
+    require('./images/thumbnail.png')
+  ],
+  description: (
+    <div>
+      <p>A dead simple Lua module to make classes easier to define.</p>
+
+      <p>Object Oriented Programming (OOP) is a commonly used paradigm across all major programming languages. While Lua has support for OOP, their implementation isn’t the easiest to work with.</p>
+
+      <p>There is no class keyword like you would come to expect from other programming languages. What Lua uses instead are called metatables, which can be tedious to work with and cause confusion. </p>
+
+      <p>Every class definition comes with a heap of overhead because of the excessive amount of work you have to do with metatables. To fix this, I made a simple module that abstracts away all of that overhead so you can focus on your code.</p>
+
+      <div className="grid__col grid__col--1-of-2">
+        <p><strong>Before</strong></p>
+
+        <img src={require('./images/before.png')} />
+      </div>
+
+      <div className="grid__col grid__col--1-of-2">
+        <p><strong>After</strong></p>
+
+        <img src={require('./images/after.png')} />
+      </div>
+    </div>
+  )
+};
--- src/jsx/routes/portfolio/index.js
+++ src/jsx/routes/portfolio/index.js
@@ -1,6 +1,7 @@
 export default [
   require('./mirage-isle').default,
   require('./apartment').default,
+  require('./class-lua').default,
   require('./echo-ridge').default,
   require('./elixir').default,
   require('./fireflies').default,
