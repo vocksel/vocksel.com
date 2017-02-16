8e96e1cc00054cba9ee8983ba46050d5bdec3426
David Minnerly <dtreeLive@hotmail.com>
2017-02-16 16:50:27 -0500

    Apply grid classes right on the elements

--- src/jsx/components/ProjectTile.jsx
+++ src/jsx/components/ProjectTile.jsx
@@ -6,7 +6,7 @@ import ProjectPreview from './ProjectPreview';
 export default class ProjectTile extends Component {
   render() {
     return (
-      <div className="ProjectTile">
+      <div className="ProjectTile grid__col grid__col--1-of-2">
         <Link to={`/projects/${this.props.slug}`}>
           <ProjectPreview name={this.props.name} image={this.props.images[0]}
             released={this.props.released} />
