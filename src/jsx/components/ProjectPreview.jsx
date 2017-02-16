39c7548551188a9986dbb0f5764b3ea6a44b03d4
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-16 15:04:08 -0500

    Stlye up the project tiles

---
+++ src/jsx/components/ProjectPreview.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class MainImage extends Component {
+  render() {
+    return (
+      <div className="ProjectPreview-image"
+        style={{ backgroundImage: `url(${this.props.image})` }} />
+    );
+  }
+}
+
+class Meta extends Component {
+  render() {
+    return (
+      <div className="ProjectPreview-meta">
+        <span className="ProjectPreview-title grid__col grid__col--2-of-3">
+          {this.props.name}
+        </span>
+
+        <span className="ProjectPreview-date grid__col grid__col--1-of-3">
+          {this.props.released}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default class ProjectPreview extends Component {
+  render() {
+    return (
+      <div className="ProjectPreview">
+        <MainImage image={this.props.image} />
+        <Meta name={this.props.name} released={this.props.released} />
+      </div>
+    );
+  }
+}
--- src/jsx/components/ProjectTile.jsx
+++ src/jsx/components/ProjectTile.jsx
@@ -1,18 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
+import ProjectPreview from './ProjectPreview';
+
 export default class ProjectTile extends Component {
   render() {
     return (
       <div className="ProjectTile">
         <Link to={`/projects/${this.props.slug}`}>
-          <h1 className="Project-title">{this.props.name}</h1>
-          <img className="ProjectTile--mainImage" src={this.props.images[0]} />
+          <ProjectPreview name={this.props.name} image={this.props.images[0]}
+            released={this.props.released} />
         </Link>
-
-        <div className="ProjectMeta">
-          <p>{this.props.released} &ndash; {this.props.tags.join(', ')}</p>
-        </div>
       </div>
     );
   }
