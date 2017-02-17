ec2d3d8bcd739fa7dbf9753ce36c3aa518742f99
David Minnerly <dtreeLive@hotmail.com>
2017-02-16 19:53:29 -0500

    Autoplay the gallery

--- src/jsx/components/ProjectGallery.jsx
+++ src/jsx/components/ProjectGallery.jsx
@@ -10,7 +10,10 @@ export default class ProjectGallery extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true
     };
 
     return (
