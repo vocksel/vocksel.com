504c93a5e6f92739729a5902445dbe4be28f1cd5
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-17 10:39:21 -0500

    Add navigation styles

--- src/jsx/components/Navigation.jsx
+++ src/jsx/components/Navigation.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+class NavLink extends Component {
+  render() {
+    return (
+      <Link className="Navigation-link" activeClassName="active" {...this.props}>
+        {this.props.children}
+      </Link>
+    );
+  }
+}
+
 export default class Navigation extends Component {
   render() {
     return (
-      <nav>
+      <nav className="Navigation">
         <ul>
-          <li><Link to='/'>Portfolio</Link></li>
-          <li><Link to='/about'>About</Link></li>
+          <li><NavLink onlyActiveOnIndex={true} to='/'>Portfolio</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
         </ul>
       </nav>
     );
