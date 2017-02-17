088d4010fdbf1e4de5d8258384460d32346ea107
David Minnerly <dtreeLive@hotmail.com>
2017-02-17 13:46:07 -0500

    Create a method for getting current age in the About component

--- src/jsx/routes/About.jsx
+++ src/jsx/routes/About.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
-const age = moment().diff('1996-07-15', 'years');
-
 export default class About extends Component {
+  getAge() {
+    return moment().diff('1996-07-15', 'years');
+  }
+
   render() {
     return (
       <div>
-        <p>I'm a {age}-year-old programming enthusiast living in the state of New York. I aim to make a career out of server administration and network security, and in my spare time I create websites and computer programs.</p>
+        <p>I'm a {this.getAge()}-year-old programming enthusiast living in the state of New York. I aim to make a career out of server administration and network security, and in my spare time I create websites and computer programs.</p>
 
         <p>I got my first taste of programming when I was 13. I started out by learning Lua on the online building game <a href={'http://roblox.com'}> Roblox</a>. Making my little blocky creations come to life was amazing to me, and it became the foundation for something more.</p>
 
