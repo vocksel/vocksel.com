216225435b6d9b504a453b5bca19bbc32e778038
David Minnerly <dtreeLive@hotmail.com>
2017-02-17 13:35:37 -0500

    Convert routes to Component classes for consistency
    These were missed when we converted our other components to classes.

--- src/jsx/routes/Home.jsx
+++ src/jsx/routes/Home.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import ProjectTile from 'jsx/components/ProjectTile';
 import portfolio from './portfolio';
 
-export default function Home() {
-  const projects = portfolio.map((project, index) =>
-    <ProjectTile key={index} name={project.name} slug={project.slug}
-      released={project.released} image={project.images[0]} />
-  );
+export default class Home extends Component {
+  render() {
+    const projects = portfolio.map((project, index) =>
+      <ProjectTile key={index} name={project.name} slug={project.slug}
+        released={project.released} image={project.images[0]} />
+    );
 
-  return (
-    <div className="portfolio">
-      {projects}
-    </div>
-  );
+    return (
+      <div className="portfolio">
+        {projects}
+      </div>
+    );
+  }
 }
