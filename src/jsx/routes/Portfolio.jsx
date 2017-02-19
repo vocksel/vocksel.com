d5e39146e77769d73e06714f54af17f0999878e7
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-19 18:38:49 -0500

    Forgot to rename Portfolio route
    When renamiing from Home to Portfolio, the class name was forgotten.

--- src/jsx/routes/Portfolio.jsx
+++ src/jsx/routes/Portfolio.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import ProjectTile from 'jsx/components/project/ProjectTile';
 import projects from './projects';
 
-export default class Home extends Component {
+export default class Portfolio extends Component {
   render() {
     const tiles = projects.map((project, index) =>
       <ProjectTile key={index} name={project.name} slug={project.slug}
