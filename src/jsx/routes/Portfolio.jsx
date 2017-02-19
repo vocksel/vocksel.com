e3187c6f14c6c59754aefe37639db130f1cf1358
David Minnerly <dtreeLive@hotmail.com>
2017-02-19 18:39:10 -0500

    Get all the project tiles from a method

--- src/jsx/routes/Portfolio.jsx
+++ src/jsx/routes/Portfolio.jsx
@@ -4,11 +4,15 @@ import ProjectTile from 'jsx/components/project/ProjectTile';
 import projects from './projects';
 
 export default class Portfolio extends Component {
-  render() {
-    const tiles = projects.map((project, index) =>
+  getProjectTiles() {
+    return projects.map((project, index) =>
       <ProjectTile key={index} name={project.name} slug={project.slug}
         released={project.released} image={project.images[0]} />
     );
+  }
+
+  render() {
+    const tiles = this.getProjectTiles();
 
     return (
       <div>
