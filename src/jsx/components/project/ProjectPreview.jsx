c14d9a75f29ef9a897d5bb962ce2e2bc16b1015c
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-20 14:32:07 -0500

    Remove meta info from ProjectPreview

--- src/jsx/components/project/ProjectPreview.jsx
+++ src/jsx/components/project/ProjectPreview.jsx
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
 
-class MainImage extends Component {
-  render() {
-    return (
-      <div className="ProjectPreview-image"
-        style={{ backgroundImage: `url(${this.props.image})` }} />
-    );
-  }
-}
-
-class Meta extends Component {
-  render() {
-    return (
-      <div className="ProjectPreview-meta">
-        {this.props.name}
-      </div>
-    );
-  }
-}
-
 export default class ProjectPreview extends Component {
   render() {
     return (
       <div className="ProjectPreview">
-        <MainImage image={this.props.image} />
-        <Meta name={this.props.name} released={this.props.released} />
+        <div className="ProjectPreview-image"
+          style={{ backgroundImage: `url(${this.props.image})` }} />
       </div>
     );
   }
