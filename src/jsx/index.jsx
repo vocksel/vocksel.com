8a1e516fb3675c4788660ffae54ccbd148168902
David Minnerly <dtreeLive@hotmail.com>
2017-02-20 10:12:56 -0500

    Merge routes with the router
    This is how routes were initially setup, and were then moves to the `routes/` folder. Recently having the 'index.jsx' file inside routes/ has been getting annoying, as it's not technically a route but it's still mixed it.
    
    Merging all of our routes back with the router means everything under routes/ is, in fact, a route.

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
+import { Router, Route, IndexRoute,  browserHistory, applyRouterMiddleware } from 'react-router';
 import { useScroll } from 'react-router-scroll';
 
-import routes from './routes';
+import App from './routes/App';
+import Portfolio from './routes/Portfolio';
+import About from './routes/About';
+import Project from './routes/Project';
 
 ReactDOM.render((
-  <Router history={browserHistory}
-    routes={routes}
-    render={applyRouterMiddleware(useScroll())} />
+  <Router history={browserHistory} render={applyRouterMiddleware(useScroll())}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Portfolio} />
+      <Route path='/about' component={About} />
+      <Route path='/projects/:projectSlug' component={Project} />
+    </Route>
+  </Router>
 ), document.getElementById('root'));
