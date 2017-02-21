ca7e771fbb032f778aa3b8a428f4dc0c629a8004
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-21 08:58:58 -0500

    Fix typo (Github to GitHub)

--- src/jsx/components/Intro.jsx
+++ src/jsx/components/Intro.jsx
@@ -7,7 +7,7 @@ export default class Intro extends Component {
         <div className="Grid-fullColumn">
           <h1 className="Intro-title">Hi! I'm David Minnerly</h1>
 
-          <p className="Intro-text">I'm a programmer, 3D modeler, and I also make websites on occasion. I frequently post about what I'm doing on <a href="https://twitter.com/voxeldavid" className="Link--twitter">Twitter <i className="fa fa-twitter" /></a> and many of my projets are open-sourced on <a href="https://github.com/vocksel" className="Link--github">Github <i className="fa fa-github" /></a>.</p>
+          <p className="Intro-text">I'm a programmer, 3D modeler, and I also make websites on occasion. I frequently post about what I'm doing on <a href="https://twitter.com/voxeldavid" className="Link--twitter">Twitter <i className="fa fa-twitter" /></a> and many of my projets are open-sourced on <a href="https://github.com/vocksel" className="Link--github">GitHub <i className="fa fa-github" /></a>.</p>
         </div>
       </section>
     );
