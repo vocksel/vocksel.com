79faca9d0e254c5d0a8c48b79318cd14fdcfb72c
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-23 10:40:45 -0500

    Apply grid container through Section styles

--- src/jsx/components/Section.jsx
+++ src/jsx/components/Section.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
 
-import grids from 'css/objects/_grids.scss';
 import styles from './Section.scss';
 
 export default class Section extends Component {
   render() {
     return (
-      <section className={classNames(styles.base, grids.grid)}>
+      <section className={styles.main}>
         {this.props.children}
       </section>
     );
