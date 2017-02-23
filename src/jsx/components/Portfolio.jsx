a5af7ec8f2e90468f074bd6e58d9c15fcb3128d0
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-23 10:13:38 -0500

    Create a class to encapsulate the section title

--- src/jsx/components/Portfolio.jsx
+++ src/jsx/components/Portfolio.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
+import grids from 'css/objects/_grids.scss';
 import Section from './Section';
 import ProjectTile from './project/ProjectTile';
+import Title from './layout/Title';
 import projects from 'jsx/routes/projects';
 
 export default class Portfolio extends Component {
@@ -16,7 +18,14 @@ export default class Portfolio extends Component {
     const tiles = this.getProjectTiles();
 
     return (
-      <Section name="Selected work">
+      <Section>
+        {/* HACK: Currently we need to wrap the title in a full-width column so
+          that it appears above the project tiles. Special components should be
+          made so we can remove this. */}
+        <div className={grids.full}>
+          <Title>Selected work</Title>
+        </div>
+
         {tiles}
       </Section>
     );
