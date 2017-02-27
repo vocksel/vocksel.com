a516a454f4c25c6148c42004f297d873273750ce
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-27 18:02:39 -0500

    Revise some words

--- src/jsx/routes/Contact.jsx
+++ src/jsx/routes/Contact.jsx
@@ -11,7 +11,7 @@ export default class About extends Component {
           <Content>
             <Title>Contact</Title>
 
-            <p>Let's talk! Feel free to message me with anything on your mind, be it a project, a question, or just to say hi. Your message will be sent straight to my inbox, and I'll get back to you as soon as I'm able to.</p>
+            <p>Let's talk! Feel free to message me with anything on your mind, be it a project, a question, or just to say hi. Your message will be sent straight to my inbox, and I'll get back to you at the Email address specified as soon as I get the chance.</p>
 
             <ContactForm />
           </Content>
