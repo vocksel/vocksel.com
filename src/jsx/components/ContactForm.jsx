4535315a2b4fa5d4e412bffb766ce4864443dda9
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-27 12:58:42 -0500

    Use AJAX to handle the form
    With this, the user is no longer redirected to the PHP script.
    
    We still have more to do before the form experience is complete, but this gets us one step closer.

--- src/jsx/components/ContactForm.jsx
+++ src/jsx/components/ContactForm.jsx
@@ -1,24 +1,81 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 import style from './ContactForm.scss';
 
 export default class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    $.ajax({
+      url: 'http://contact.davidminnerly.com',
+      type: 'POST',
+      data: {
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message
+      }
+    })
+    .done(() => alert('Form sent!'));
+  }
+
   render() {
     return (
-      <form action="http://contact.davidminnerly.com" method="post" className={style.base}>
+      <form onSubmit={this.handleSubmit} className={style.base}>
         <div className={style.userField}>
           <label htmlFor="name">Full name:</label>
-          <input id="name" type="text" name="name" placeholder="John Doe" />
+          <input
+            required
+            id="name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={this.state.name}
+            onChange={this.handleInputChange} />
         </div>
 
         <div className={style.userField}>
           <label htmlFor="email">Email:</label>
-          <input id="email" type="email" name="email" placeholder="john@example.com" />
+          <input
+            required
+            id="email"
+            type="email"
+            name="email"
+            placeholder="john@example.com"
+            value={this.state.email}
+            onChange={this.handleInputChange} />
         </div>
 
         <div className={style.messageField}>
           <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" placeholder="What's on your mind?"></textarea>
+          <textarea
+            required
+            id="message"
+            name="message"
+            placeholder="What's on your mind?"
+            value={this.state.message}
+            onChange={this.handleInputChange} />
         </div>
 
         <div className={style.submit}>
