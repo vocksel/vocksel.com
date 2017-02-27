cb0ef782d2572d825b7f3903cbea83125815fed0
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-27 13:32:26 -0500

    Fix up navigation
    The nav got a little messed up from the changes to the `half` variant of Content.
    
    We don't have time to implement a robust solution, so for right now we're just hacking something together so it displays a bit nicer.

--- src/jsx/components/Header.jsx
+++ src/jsx/components/Header.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router';
 
 import nav from './navigation/NavLink.scss';
 import styles from './Header.scss';
-import { Section, Content } from './layout';
+import { Section } from './layout';
 import PrimaryNav from './navigation/PrimaryNav';
 
 export default class Header extends Component {
   render() {
     return (
       <Section className={styles.base}>
-        <Content half>
+        <div className={styles.name}>
           <Link to="/" className={nav.link}>David Minnerly</Link>
-        </Content>
+        </div>
 
-        <Content half>
+        <div className={styles.nav}>
           <PrimaryNav />
-        </Content>
+        </div>
       </Section>
     );
   }
