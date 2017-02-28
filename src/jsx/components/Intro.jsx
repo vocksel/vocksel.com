7b8ad3224d5e267de0c24252efb4f221c2715603
Marin Minnerly <dtreeLive@hotmail.com>
2017-02-28 09:49:03 -0500

    Fix a critical typo
    "projets" to "projects". Phew, that was close. My brand was almost tarnished by such a simple mistake

--- src/jsx/components/Intro.jsx
+++ src/jsx/components/Intro.jsx
@@ -13,7 +13,7 @@ export default class Intro extends Component {
 
           <p>Hey, nice to meet you, I'm David Minnerly. I'm a programmer, 3D modeler, and I also make websites on occasion.</p>
 
-          <p>I'm fluent in Lua, and I can hold my ground in JavaScript and Python. I frequently post about what I'm up to on <a href="https://twitter.com/voxeldavid" className={links.twitter}>Twitter <i className="fa fa-twitter" /></a> and many of my projets are open-sourced on <a href="https://github.com/vocksel" className={links.github}>GitHub <i className="fa fa-github" /></a>.</p>
+          <p>I'm fluent in Lua, and I can hold my ground in JavaScript and Python. I frequently post about what I'm up to on <a href="https://twitter.com/voxeldavid" className={links.twitter}>Twitter <i className="fa fa-twitter" /></a> and many of my projects are open-sourced on <a href="https://github.com/vocksel" className={links.github}>GitHub <i className="fa fa-github" /></a>.</p>
 
           <p>Feel free to <Link to="/contact" className={links.contact}>contact me <i className="fa fa-paper-plane-o" /></Link> with anything on your mind, I'd love to chat and get to know you.</p>
         </Content>
