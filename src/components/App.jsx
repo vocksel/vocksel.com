53d04a79a654b1a6c5e6f5699336a59f12fcf2a5
David Minnerly <voxeldavid@gmail.com>
2019-11-28 22:17:58 -0500

    Use tab indentation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import style from './App.scss';
 console.log(style.app);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <p className={style.app}>Hello, World!</p>
-    );
-  }
+	render() {
+		return (
+			<p className={style.app}>Hello, World!</p>
+		);
+	}
 }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,73 +5,73 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
-  mode: IS_PRODUCTION ? 'production' : 'development',
-  devtool: 'source-map',
+	entry: path.resolve(__dirname, 'src/index.js'),
+	mode: IS_PRODUCTION ? 'production' : 'development',
+	devtool: 'source-map',
 
-  resolve: {
-    modules: [
-      path.resolve(__dirname, 'src'),
-      path.resolve(__dirname, 'node_modules')
-    ],
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'src'),
+			path.resolve(__dirname, 'node_modules')
+		],
 
-    extensions: [
-      '.js', '.json', // Defaults
-      '.jsx'
-    ]
-  },
+		extensions: [
+			'.js', '.json', // Defaults
+			'.jsx'
+		]
+	},
 
-  plugins: [
-    new CleanWebpackPlugin(),
+	plugins: [
+		new CleanWebpackPlugin(),
 
-    new HtmlWebpackPlugin({
-      title: 'David Minnerly',
-      favicon: path.resolve(__dirname, 'src/favicon.ico'),
-      meta: {
-        viewport: 'width=device-width, initial-scale=1, user-scalable=no'
-      }
-    }),
-  ],
+		new HtmlWebpackPlugin({
+			title: 'David Minnerly',
+			favicon: path.resolve(__dirname, 'src/favicon.ico'),
+			meta: {
+				viewport: 'width=device-width, initial-scale=1, user-scalable=no'
+			}
+		}),
+	],
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          'eslint-loader',
-        ]
-      },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					'babel-loader',
+					'eslint-loader',
+				]
+			},
 
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          'sass-loader',
-        ]
-      },
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					},
+					'sass-loader',
+				]
+			},
 
-      // We write everything in Sass, CSS handling is just for third-party libraries.
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      }
-    ]
-  },
+			// We write everything in Sass, CSS handling is just for third-party libraries.
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				]
+			}
+		]
+	},
 
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
 };
