2b42ad6f14e3cba58e8d95654db299fd4d8e03fe
David Minnerly <voxeldavid@gmail.com>
2019-11-29 12:34:02 -0500

    Fix project indentation

--- src/projects/apartment/index.jsx
+++ src/projects/apartment/index.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 
 export default {
-  name: 'Rhinebeck Apartment',
-  slug: 'apartment',
-  released: '2016',
-  tags: [ '3D Modeling', 'Roblox' ],
-  images: [
-    require('./images/living-room.jpg'),
-    require('./images/bedroom.jpg'),
-    require('./images/desk.jpg'),
-    require('./images/bed.jpg'),
-    require('./images/bookcase.jpg'),
-    require('./images/outside.jpg')
-  ],
+	name: 'Rhinebeck Apartment',
+	slug: 'apartment',
+	released: '2016',
+	tags: [ '3D Modeling', 'Roblox' ],
+	images: [
+		require('./images/living-room.jpg'),
+		require('./images/bedroom.jpg'),
+		require('./images/desk.jpg'),
+		require('./images/bed.jpg'),
+		require('./images/bookcase.jpg'),
+		require('./images/outside.jpg')
+	],
 
-  description: (
-    <div>
-      <p>A high-end apartment based in Rhinebeck, New York.</p>
+	description: (
+		<div>
+			<p>A high-end apartment based in Rhinebeck, New York.</p>
 
-      <p>My projects prior to this apartment have only consisted of light 3D modeling. I wanted to challenge myself and make the apartment something special, and to do this I spent a little over 4 months creating 50+ models to furnish the living space I created.</p>
+			<p>My projects prior to this apartment have only consisted of light 3D modeling. I wanted to challenge myself and make the apartment something special, and to do this I spent a little over 4 months creating 50+ models to furnish the living space I created.</p>
 
-      <p>Some of the models are large and easy to spot, others you might not notice at all. Even if some models are not looked at closely enough to examine the details, or if they never get seen at all, they still contribute to the overall &ldquo;lived in&rdquo; feeling I wanted to convey.</p>
+			<p>Some of the models are large and easy to spot, others you might not notice at all. Even if some models are not looked at closely enough to examine the details, or if they never get seen at all, they still contribute to the overall &ldquo;lived in&rdquo; feeling I wanted to convey.</p>
 
-      <p>I love games where detail is packed into every nook and cranny. Where there's so much to see that you can't possibly see everything in a single playthrough. Books filled with extensive lore, an assortment of unique items and props as far as the eye can see, and numerous storylines that you may never uncover. I love that feeling of <em>knowing</em> that I'm surrounded by all these intricate details, even if I never notice them.</p>
+			<p>I love games where detail is packed into every nook and cranny. Where there's so much to see that you can't possibly see everything in a single playthrough. Books filled with extensive lore, an assortment of unique items and props as far as the eye can see, and numerous storylines that you may never uncover. I love that feeling of <em>knowing</em> that I'm surrounded by all these intricate details, even if I never notice them.</p>
 
-      <p>While the apartment is not at that level of detail, the desire to create something packed with intricacy and sentimental value was a driving force as I created this living space.</p>
-    </div>
-  )
+			<p>While the apartment is not at that level of detail, the desire to create something packed with intricacy and sentimental value was a driving force as I created this living space.</p>
+		</div>
+	)
 };
--- src/projects/index.js
+++ src/projects/index.js
@@ -1,10 +1,10 @@
 export default [
-  require('./mirage-isle').default,
-  require('./apartment').default,
-  require('./repo-import').default,
-  require('./class-lua').default,
-  require('./echo-ridge').default,
-  require('./elixir').default,
-  require('./fireflies').default,
-  require('./snowy-mountain').default
+	require('./mirage-isle').default,
+	require('./apartment').default,
+	require('./repo-import').default,
+	require('./class-lua').default,
+	require('./echo-ridge').default,
+	require('./elixir').default,
+	require('./fireflies').default,
+	require('./snowy-mountain').default
 ];
