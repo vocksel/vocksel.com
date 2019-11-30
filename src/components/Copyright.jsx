ae132857585956b9e9c4d820d82dbbb3a8856cba
Marin Minnerly <me@vocksel.com>
2019-11-30 15:32:02 -0500

    Make the copyright year dynamic
    It'll never need to be edited again

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Copyright extends React.Component {
+	getYear() {
+		return new Date().getFullYear();
+	}
+
+	render() {
+		return <span>&copy; {this.getYear()} David Minnerly</span>;
+	}
+}
