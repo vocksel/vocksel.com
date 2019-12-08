c92d7fe3bb935101a6ed0ef2379c0443b6a5ae99
Marin Minnerly <me@vocksel.com>
2019-12-08 16:40:47 -0500

    Revise experiences

--- src/experiences.js
+++ src/experiences.js
@@ -1,6 +1,7 @@
 // Contains data for past work experience
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Company from './Company';
 
 const novaly = new Company('Novaly Studios', 'https://twitter.com/novalystudios');
@@ -18,7 +19,7 @@ const experiences = [
 	{
 		job: 'Software Engineering Intern',
 		company: roblox,
-		description: 'Worked to remove bloat and set best-practices in-house with my knowledge of the Roblox platform.',
+		description: '10 weeks of working in house at Roblox HQ on the application layer of the platform. Removed bloat, helped set best practices, and worked with full-time employees to make their existing workflows better.',
 		wasInHouse: true,
 		startDate: new Date(2019, 5),
 		endDate: new Date(2019, 7),
@@ -29,14 +30,14 @@ const experiences = [
 		company: novaly,
 		description: <span>Created the Robbery feature for <a href='https://www.roblox.com/games/2563455047/Bandit-Simulator'>Bandit Simulator</a> and squashed bugs.</span>,
 		wasInHouse: false,
-		startDate: new Date(2019, 5),
-		endDate: new Date(2019, 7),
+		startDate: new Date(2018, 10),
+		endDate: new Date(2019, 0),
 	},
 
 	{
 		job: 'Level Design contractor',
 		company: roblox,
-		description: 'Worked for Roblox to create the Aquaman game for a Warner Brother\'s sponsorship.',
+		description: <span>Worked for Roblox to create <Link to='/projects/aquaman'>Aquaman: City of Rolantis</Link> for a Warner Brother's sponsorship.</span>,
 		wasInHouse: false,
 		startDate: new Date(2018, 6),
 		endDate: new Date(2019, 10),
