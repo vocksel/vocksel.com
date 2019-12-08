7d0bf5374c1b681fc248920059f61f4fa6062c9f
Marin Minnerly <me@vocksel.com>
2019-12-07 23:19:01 -0500

    Add subtitles for projects

--- src/projects/apartment/index.jsx
+++ src/projects/apartment/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ProjectType } from 'types';
 import thumbnail from './images/living-room.jpg';
+import bedroom from './images/bedroom.jpg';
+import outside from './images/outside.jpg';
 
 export default {
-	title: 'Rhinebeck Apartment',
+	title: 'Rhinebeck',
+	subtitle: 'An intimate, high-end apartment based in Rhinebeck, NY',
 	slug: 'apartment',
 	releaseDate: new Date(2016, 0),
 	type: ProjectType.Game,
@@ -11,11 +14,13 @@ export default {
 	url: 'https://www.roblox.com/games/343113123/Rhinebeck',
 	description: (
 		<div>
-			<p>A high-end apartment based in Rhinebeck, New York.</p>
+			<p>My projects prior to Rhinebeck have only consisted of light 3D modeling, so I wanted to challenge myself and make the apartment something special, and to do this I spent a little over 4 months creating 50+ models to furnish the living space I created. Some of the models are large and easy to spot, others you might not notice at all. Even if some models are not looked at closely enough to examine the details, or if they never get seen at all, they still contribute to the overall &ldquo;lived in&rdquo; feeling I wanted to convey.</p>
 
-			<p>My projects prior to this apartment have only consisted of light 3D modeling. I wanted to challenge myself and make the apartment something special, and to do this I spent a little over 4 months creating 50+ models to furnish the living space I created. Some of the models are large and easy to spot, others you might not notice at all. Even if some models are not looked at closely enough to examine the details, or if they never get seen at all, they still contribute to the overall &ldquo;lived in&rdquo; feeling I wanted to convey.</p>
+			<img src={bedroom} alt=""/>
 
 			<p>I love games where detail is packed into every nook and cranny. Where there's so much to see that you can't possibly see everything in a single playthrough. Books filled with extensive lore, an assortment of unique items and props as far as the eye can see, and numerous storylines that you may never uncover. I love that feeling of <em>knowing</em> that I'm surrounded by all these intricate details, even if I never notice them. While the apartment is not at that level of detail, the desire to create something packed with intricacy and sentimental value was a driving force as I created this living space.</p>
+
+			<img src={outside} alt=""/>
 		</div>
 	)
 };
--- src/projects/aquaman/index.jsx
+++ src/projects/aquaman/index.jsx
@@ -3,16 +3,18 @@ import { ProjectType } from 'types';
 import thumbnail from './thumbnail.jpg';
 
 export default {
-	title: 'Aquaman: City of Rolantis',
+	title: 'Aquaman',
+	subtitle: 'City of Rolantis',
 	slug: 'aquaman',
 	startDate: new Date(2018, 6),
 	releaseDate: new Date(2018, 10),
 	type: ProjectType.Game,
 	thumbnail: thumbnail,
 	url: 'https://www.roblox.com/games/2056459358/City-of-Rolantis',
-	shortDescription: 'A hectic 4 month sprint to release a game for the Warner Brother\'s sponsorship',
 	description: (
 		<React.Fragment>
+			<p>A hectic 4 month sprint to release a game for the Warner Brother's sponsorship.</p>
+
 			<p>Hello, World!</p>
 
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quam soluta <a href="#">tempora harum</a> ut aut vero fuga similique necessitatibus nostrum quas doloremque, minus error ipsam ullam, blanditiis itaque ratione eaque. Lorem ipsum dolor sit amet consectetur adipisicing elit.	<a href="#">Voluptatem maiores</a> fugiat nam quibusdam accusamus officia aliquid tempore, ea suscipit illum.</p>
