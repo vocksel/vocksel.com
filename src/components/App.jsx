09baea6f9aa891891cd4992c1caa331c7ad901fb
David Minnerly <voxeldavid@gmail.com>
2019-12-07 20:33:18 -0500

    Create a component for the flag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import Copyright from './Copyright';
 import Home from './Home';
 import HorizontalList from './HorizontalList';
 import ProjectDetail from './ProjectDetail';
+import TransFlag from './TransFlag';
 import bulma from 'bulma.scss';
-import flag from 'transflag.png';
 export default class App extends React.Component {
 	render() {
 		return (
@@ -22,7 +22,7 @@ export default class App extends React.Component {
 
 							<a href="https://github.com/vocksel/davidminnerly.com">Source</a>
 
-							<img src={flag} title={'I know the world is scary, but you\'re doing great <3'} alt='Trans Flag'/>
+							<TransFlag />
 						</HorizontalList>
 					</footer>
 				</div>
---
+++ src/components/TransFlag.jsx
@@ -0,0 +1,12 @@
+// Needed to make a component for this so I could style it so it could be
+// aligned properly in the footer.
+
+import React from 'react';
+import style from './TransFlag.scss';
+import flag from 'transflag.png';
+
+const TransFlag = () => (
+	<img src={flag} className={style.main} title={'I know the world is scary, but you\'re doing great <3'} alt='Trans Flag' />
+);
+
+export default TransFlag;
