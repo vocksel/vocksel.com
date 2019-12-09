0fb4841567d461363054b954a3fbbec83e89f5cf
Marin Minnerly <me@vocksel.com>
2019-12-08 20:17:11 -0500

    Make reuseable CaptionImage component

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -2,37 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import bulma from '../bulma.scss';
+import CaptionedImage from './CaptionedImage';
 import style from './ProjectTile.scss';
 
 export default class ProjectTile extends React.Component {
 	static propTypes = {
+		className: PropTypes.string,
 		project: PropTypes.shape({
 			slug: PropTypes.string,
 			title: PropTypes.string,
 			shortDescription: PropTypes.string,
 			thumbnail: PropTypes.string,
 			description: PropTypes.element,
-		})
+		}),
 	};
 
 	render() {
 		const { project } = this.props;
 
 		return (
-			<div className={classNames(bulma.column, bulma['is-one-third'])}>
+			<div className={classNames(style.container, this.props.className)}>
 				<Link to={`/projects/${project.slug}/`} className={style.link}>
-					<div className={style.container}>
-						<div className={style.border}></div>
-
-						<div className={style.display}>
-							<img className={style.image} src={project.thumbnail} />
-
-							<div className={style.caption}>
-								<h2 className={style.title}>{project.title}</h2>
-							</div>
-						</div>
-					</div>
+					<CaptionedImage className={style.image} src={project.thumbnail} caption={project.title} />
 				</Link>
 			</div>
 		);
