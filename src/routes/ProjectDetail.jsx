a082c96aed95daf8e480d4e16b1b6d377a2217ad
David Minnerly <voxeldavid@gmail.com>
2019-12-11 17:19:30 -0500

    Restructure bulma classes

--- src/routes/ProjectDetail.jsx
+++ src/routes/ProjectDetail.jsx
@@ -22,20 +22,22 @@ const ProjectDetail = () => {
 
 	return (
 		<div className={bulma.section}>
-			<h1 className={style.title}>{project.title} <span className={style.date}> &mdash; {project.releaseDate.getFullYear()}</span></h1>
-			<p className={style.subtitle}>{project.subtitle}</p>
+			<div className={bulma.container}>
+				<h1 className={style.title}>{project.title} <span className={style.date}> &mdash; {project.releaseDate.getFullYear()}</span></h1>
+				<p className={style.subtitle}>{project.subtitle}</p>
 
-			<img src={project.thumbnail} alt="" />
+				<img src={project.thumbnail} alt="" />
 
-			<section className={style.description}>
-				{project.description}
-			</section>
+				<section className={style.description}>
+					{project.description}
+				</section>
 
-			<HorizontalList isCentered>
-				{getProjectLink(index-1, '< Prev')}
-				<Link to="/">Home</Link>
-				{getProjectLink(index+1, 'Next >')}
-			</HorizontalList>
+				<HorizontalList isCentered>
+					{getProjectLink(index-1, '< Prev')}
+					<Link to="/">Home</Link>
+					{getProjectLink(index+1, 'Next >')}
+				</HorizontalList>
+			</div>
 		</div>
 	);
 };
