06480babe7f1eeb3e36e54e2c0a0250ab2e12752
Marin Minnerly <me@vocksel.com>
2019-12-13 18:54:50 -0500

    Tidy up the ProjectList route

--- src/routes/ProjectList.jsx
+++ src/routes/ProjectList.jsx
@@ -1,17 +1,19 @@
-import classNames from 'classnames';
 import React from 'react';
 import projects from 'projects';
 import ProjectTile from 'components/ProjectTile';
 import bulma from 'bulma.scss';
+import style from './ProjectList.scss';
 
 const ProjectList = () => {
 	const tiles = projects.map(project =>
-		<ProjectTile className={classNames(bulma.column, bulma['is-half'])} key={project.slug} project={project} />
+		<ProjectTile slide='x' className={style.tile} key={project.slug} project={project} />
 	);
 
 	return (
-		<div className={classNames(bulma.columns, bulma['is-multiline'])}>
-			{tiles}
+		<div className={bulma.section}>
+			<div className={bulma.container}>
+				{tiles}
+			</div>
 		</div>
 	);
 };
