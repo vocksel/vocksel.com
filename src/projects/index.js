e7ba0453ab216fb2f5e5a91e6686d6fd3e4fba63
David Minnerly <voxeldavid@gmail.com>
2019-12-14 18:25:11 -0500

    Hide some projects

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,9 +1,9 @@
 export default [
-	require('./aquaman').default,
+	// require('./aquaman').default,
 	require('./wild-revolvers').default,
 	require('./rhinebeck').default,
-	require('./class-lua').default,
 	require('./mirage-isle').default,
+	// require('./class-lua').default,
 	// require('./repo-import').default,
 	// require('./echo-ridge').default,
 	// require('./elixir').default,
