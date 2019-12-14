da8fda5ae018cf6256c5c37a57eeb08586e3190f
David Minnerly <voxeldavid@gmail.com>
2019-12-13 21:11:31 -0500

    Add navigation!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Copyright from './Copyright';
 import HorizontalList from './HorizontalList';
 import Home from 'routes/Home';
@@ -9,11 +9,25 @@ import NoMatch from 'routes/NoMatch';
 import ScrollToTop from './ScrollToTop';
 import TransFlag from './TransFlag';
 import generic from 'generic.scss';
+import bulma from 'bulma.scss';
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<Router>
 				<ScrollToTop />
+
+				<header className={bulma.section}>
+					<div className={bulma.container}>
+						<nav>
+							<HorizontalList isRightAligned>
+								<Link to='/'>Home</Link>
+								<Link to='/projects'>Projects</Link>
+								<Link to='/about'>About</Link>
+							</HorizontalList>
+						</nav>
+					</div>
+				</header>
 
 				<Switch>
 					<Route path='/projects/:projectId'><ProjectDetail /></Route>
--- src/components/HorizontalList.jsx
+++ src/components/HorizontalList.jsx
@@ -6,11 +6,15 @@ import style from './HorizontalList.scss';
 export default class HorizontalList extends React.Component {
 	static propTypes = {
 		isCentered: PropTypes.bool,
+		isRightAligned: PropTypes.bool,
 	};
 
 	render() {
+		const isCentered = this.props.isCentered ? style.isCentered : '';
+		const isRightAligned = this.props.isRightAligned ? style.isRightAligned : '';
+
 		return (
-			<ul className={classNames(style.list, this.props.isCentered ? style.isCentered : '')}>
+			<ul className={classNames(style.list, isCentered, isRightAligned)}>
 				{React.Children.map(this.props.children, item => <li>{item}</li>)}
 			</ul>
 		);
