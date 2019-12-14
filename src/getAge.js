6c9ebb55e12b6ecdffca6894312f74a76a704c52
Marin Minnerly <me@vocksel.com>
2019-12-14 11:11:41 -0500

    Reduce prod bundle size from 1.32 MB to 526 KB

--- src/getAge.js
+++ src/getAge.js
@@ -1,5 +1,17 @@
-import moment from 'moment';
-
 export default function getAge() {
-	return moment().diff('1996-07-15', 'years');
+	// Note that this might have some precision issues.
+	//
+	// This answer is where I got the idea from:
+	// https://stackoverflow.com/a/20669357
+	//
+	// However this answer in a different thread might yield better results. It
+	// works in days, however, and I didn't want to think about how I would
+	// convert that to years, so we're just rolling with the current solution.
+	// https://stackoverflow.com/a/15289883
+
+	const birthday = new Date('7/15/1996');
+	const today = new Date();
+	const diff = new Date(today.getTime() - birthday.getTime());
+
+	return diff.getUTCFullYear()  - 1970;
 }
