ead6fadc342df8316bc7bbcd7eb8bee7895b80a2
Marin Minnerly <me@vocksel.com>
2019-12-14 10:27:38 -0500

    Use a burger menu on tablet and mobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,53 @@
 import classNames from 'classnames';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import Copyright from './Copyright';
 import HorizontalList from './HorizontalList';
 import Home from 'routes/Home';
 import ProjectDetail from 'routes/ProjectDetail';
 import ProjectList from 'routes/ProjectList';
 import NoMatch from 'routes/NoMatch';
+import BurgerMenu from './nav/BurgerMenu';
+import LineMenu from './nav/LineMenu';
 import ScrollToTop from './ScrollToTop';
 import TransFlag from './TransFlag';
 import generic from 'generic.scss';
 import bulma from 'bulma.scss';
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<Router>
-				<ScrollToTop />
+export default function App() {
+	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
 
-				<header className={bulma.section}>
-					<div className={bulma.container}>
-						<nav>
-							<HorizontalList isRightAligned>
-								<Link to='/'>Home</Link>
-								<Link to='/projects'>Projects</Link>
-								<Link to='/about'>About</Link>
-							</HorizontalList>
-						</nav>
-					</div>
-				</header>
+	return (
+		<Router>
+			<ScrollToTop />
 
-				<Switch>
-					<Route path='/projects/:projectId'><ProjectDetail /></Route>
-					<Route path='/projects'><ProjectList /></Route>
-					<Route exact path='/'><Home /></Route>
-					<Route path='*'><NoMatch /></Route>
-				</Switch>
+			{isTabletOrMobile && <BurgerMenu />}
 
-				<footer className={classNames(bulma.section, generic.finePrint)}>
-					<HorizontalList isCentered>
-						<Copyright />
+			<header className={bulma.section}>
+				<div className={bulma.container}>
+					{!isTabletOrMobile &&
+						<LineMenu />
+					}
+				</div>
+			</header>
 
-						<a href="https://github.com/vocksel/davidminnerly.com">Source</a>
+			<Switch>
+				<Route path='/projects/:projectId'><ProjectDetail /></Route>
+				<Route path='/projects'><ProjectList /></Route>
+				<Route exact path='/'><Home /></Route>
+				<Route path='*'><NoMatch /></Route>
+			</Switch>
 
-						<TransFlag />
-					</HorizontalList>
-				</footer>
-			</Router>
-		);
-	}
+			<footer className={classNames(bulma.section, generic.finePrint)}>
+				<HorizontalList isCentered>
+					<Copyright />
+
+					<a href="https://github.com/vocksel/davidminnerly.com">Source</a>
+
+					<TransFlag />
+				</HorizontalList>
+			</footer>
+		</Router>
+	);
 }
---
+++ src/components/nav/BurgerMenu.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Menu from 'react-burger-menu/lib/menus/slide';
+import { Link } from 'react-router-dom';
+import './BurgerMenu.scss';
+
+// Everything going on with the state is to make the menu work with
+// react-router-dom. Since the page isn't actually changing, we have to close
+// the menu ourselves when a link is clicked.
+
+export default class BurgerMenu extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isOpen: false
+		};
+	}
+
+	handleStateChange(state) {
+		this.setState({ isOpen: state.isOpen });
+	}
+
+	closeMenu() {
+		this.setState({ isOpen: false });
+	}
+
+	render() {
+		return (
+			<Menu right
+				isOpen={this.state.isOpen}
+				onStateChange={(state) => this.handleStateChange(state)}
+			>
+				<Link onClick={() => this.closeMenu()} to='/'><i className='fas fa-home' /> Home</Link>
+				<Link onClick={() => this.closeMenu()} to='/projects/'><i className='fas fa-heart' /> Projects</Link>
+				<Link onClick={() => this.closeMenu()} to='/about/'><i className='fas fa-question-circle' /> About</Link>
+			</Menu>
+		);
+	}
+}
