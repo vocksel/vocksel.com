051d8077502cd48d242058449a53f91d4e04d516
David Minnerly <voxeldavid@gmail.com>
2019-12-14 10:27:38 -0500

    Use a burger menu on tablet and mobile

---
+++ src/components/nav/LineMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import HorizontalList from '../HorizontalList';
+
+export default function LineMenu() {
+	return (
+		<nav>
+			<HorizontalList isRightAligned>
+				<Link to='/'>Home</Link>
+				<Link to='/projects'>Projects</Link>
+				<Link to='/about'>About</Link>
+			</HorizontalList>
+		</nav>
+	);
+}
