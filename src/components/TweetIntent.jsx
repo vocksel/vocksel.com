4ca17be5fcb9bbea0d99722c6bbc6a18643c9c28
Marin Minnerly <me@vocksel.com>
2019-12-16 21:08:29 -0500

    Add a tweet button

---
+++ src/components/TweetIntent.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import style from './TweetIntent.scss';
+
+export default class TweetIntent extends React.Component {
+	BASE_URL = 'https://twitter.com/intent/tweet';
+
+	static propTypes = {
+		text: PropTypes.string.isRequired,
+		url: PropTypes.string,
+		via: PropTypes.string,
+	}
+
+	static defaultProps = {
+		via: 'vocksel_',
+	}
+
+	render() {
+		const { text, via } = this.props;
+
+		let url = this.props.url;
+		if (!url) {
+			url = location.href;
+		}
+
+		let intentUrl = `${this.BASE_URL}?text=${text}&url=${url}`;
+
+		if (via) {
+			intentUrl = intentUrl + `&via=${via}`;
+		}
+
+		return <a className={style.button} href={encodeURI(intentUrl)}>{this.props.children} <i className='fab fa-twitter' /></a>;
+	}
+}
