31308d1c4bb6d351546c3c12140bf7dac2e402e8
Marin Minnerly <me@vocksel.com>
2020-08-01 20:37:22 -0400

    Fix prop validation error

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,11 @@
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
 import React from 'react';
 import style from './Button.scss';
 
 export default class Button extends React.Component {
-	static propTypes = {
-		text: PropTypes.string.isRequired,
-		url: PropTypes.string.isRequired,
-	};
-
 	render() {
-		const { url } = this.props;
-
 		return (
-			<div href={url} className={classNames(style.button, style.outline )}>
+			<div className={classNames(style.button, style.outline )}>
 				{this.props.children}
 			</div>
 		);
