4e990dcfc1db830fd346faba7db3bed0bb84d104
David Minnerly <voxeldavid@gmail.com>
2021-07-24 12:49:26 -0400

    Apply eslint fixes

--- deploy.js
+++ deploy.js
@@ -13,9 +13,9 @@
 //   # how they'll look online before being fully ready to push to production.
 //   $ npm run deploy:dev
 
-const fs = require('fs');
-const git = require('simple-git');
-const config = require('./webpack.config.js');
+const fs = require('fs')
+const git = require('simple-git')
+const config = require('./webpack.config.js')
 
 // Remote repositories for deploying the site. These can only be pushed to with
 // proper authorization.
@@ -30,28 +30,28 @@ const REMOTES = {
 	},
 }
 
-const OUTPUT_DIR = config.output.path || 'dist';
+const OUTPUT_DIR = config.output.path || 'dist'
 
 function getRemote() {
 	if (process.env.NODE_ENV == 'production') {
-		return REMOTES.production;
+		return REMOTES.production
 	} else {
-		return REMOTES.staging;
+		return REMOTES.staging
 	}
-};
+}
 
 if (fs.existsSync(OUTPUT_DIR)) {
 	const repo = git(OUTPUT_DIR)
-	.init()
-	.add('.')
-	.commit('Release');
+		.init()
+		.add('.')
+		.commit('Release')
 
-	const remote = getRemote();
+	const remote = getRemote()
 
-	repo.addRemote('origin', remote.repo);
-	repo.push(['-f', 'origin', 'master']);
+	repo.addRemote('origin', remote.repo)
+	repo.push(['-f', 'origin', 'master'])
 
-	console.log('Deployed to ' + remote.url);
+	console.log('Deployed to ' + remote.url)
 } else {
-	console.warn('Nothing has been built. Run `npm run build` first');
+	console.warn('Nothing has been built. Run `npm run build` first')
 }
