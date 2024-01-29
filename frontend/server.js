51223ae85e8443c2366d30de8bc8744aff8f529b
Marin Minnerly <me@vocksel.com>
2024-01-29 07:12:45 -0800

    Rewrite URLs for ReactRouter

--- frontend/server.js
+++ frontend/server.js
@@ -2,5 +2,12 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static('dist'))
+
+app.get('*', (_req, res) => {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
 app.listen(3000)
+
+console.log("Server started on localhost:3000")
