933f7abf1799bb892957c639b51ca9a390e856fd
Marin Minnerly <me@vocksel.com>
2025-05-04 20:11:55 -0700

    Fix text color for copyright

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -2,5 +2,5 @@ import { useMemo } from "react";
 
 export default function Copyright() {
 	const year = useMemo(() => new Date().getFullYear(), []);
-	return <span>&copy; {year} Marin Minnerly</span>;
+	return <span className="text-zinc-200">&copy; {year} Marin Minnerly</span>;
 }
