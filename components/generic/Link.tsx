11009972dc0072da0c8c35d0f4a6c9ebd596b2d6
Marin Minnerly <me@vocksel.com>
2025-05-04 20:14:39 -0700

    Open external links in new tab

--- components/generic/Link.tsx
+++ components/generic/Link.tsx
@@ -7,11 +7,14 @@ type Props = {
 };
 
 export default function Link({ href, title, children }: Props) {
+	const isExternal = !href.startsWith("/");
+
 	return (
 		<NextLink
 			className="text-blue-400 hover:text-blue-300 hover:underline"
 			href={href}
 			title={title}
+			target={isExternal ? "_blank" : undefined}
 		>
 			{children}
 		</NextLink>
