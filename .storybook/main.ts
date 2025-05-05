79630a381fabadb88a9d8f2451fcc7cc62973ec2
Marin Minnerly <me@vocksel.com>
2025-05-04 21:46:02 -0700

    Fix storybook alias

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -24,7 +24,7 @@ const config: StorybookConfig = {
 			...config,
 			resolve: {
 				alias: {
-					"@": path.resolve(__dirname, "../"),
+					"@": path.resolve(process.cwd()),
 				},
 			},
 		};
